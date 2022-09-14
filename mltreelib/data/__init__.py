e9f7ff7ecd00856f1d8d69756008d51f1b512c2a
Jitin Kapila <jitin.kapila@gmail.com>
2022-09-15 00:00:17 +0530

    Add some transformative code

--- mltreelib/data/__init__.py
+++ mltreelib/data/__init__.py
@@ -1,2 +1,3 @@
+from .datasets import get_dataset, show_data
 from .data import Data
-from .utility import get_dataset,show_data
+from .utility import IndexMapper, summary_func , VariableTransformation, GroupedVariableTransformation
